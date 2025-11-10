5453987bf01a42510367e1dd25cddd22b6685874
unknown <2DAW-vjimmar0508@IESMARTINEZM.ES>
2025-11-10 13:26:11 +0100

    Subida 10/11/2025

--- UDI 1/Actividades/Actividades_basicas/40.js
+++ UDI 1/Actividades/Actividades_basicas/40.js
@@ -1,12 +1,28 @@
-"use strict"
+"use strict";
 {
-  fechaUser = prompt("Escribe una fecha de nacimiento", "d/m/Y");
-  let date = new Date(date);
-    function  mostrarEdad(date){
+  let fechaUser = prompt("Escribe una fecha de nacimiento", "dd/mm/yyyy");
 
-        year = 2025 - date.getFullYear(); 
-        return year;
+  // Separar la fecha en partes
+  let partes = fechaUser.split("/");
+  let dia = parseInt(partes[0], 10);
+  let mes = parseInt(partes[1], 10) - 1;
+  let anio = parseInt(partes[2], 10);
+
+  let date = new Date(anio, mes, dia);
+
+  function mostrarEdad(fecha) {
+    let hoy = new Date();
+    let edad = hoy.getFullYear() - fecha.getFullYear();
+
+    if (
+      hoy.getMonth() < fecha.getMonth() ||
+      (hoy.getMonth() === fecha.getMonth() && hoy.getDate() < fecha.getDate())
+    ) {
+      edad--;
     }
 
-    console.log(mostrarEdad(date));
+    return edad;
+  }
+
+  console.log("Tu edad es:", mostrarEdad(date));
 }
---
+++ UDI 1/Actividades/Actividades_basicas/41.js
@@ -0,0 +1,9 @@
+"use strict"
+{
+    function numeroAleatorio() {
+        return Math.floor(Math.random() * 100) + 501;
+      }
+      
+      console.log(numeroAleatorio());
+      
+}
