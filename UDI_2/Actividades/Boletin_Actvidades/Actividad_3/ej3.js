87cbd3e15fc9e0858863d11502bfdb0885f9cd21
unknown <2DAW-vjimmar0508@IESMARTINEZM.ES>
2025-11-27 14:16:18 +0100

    Subida 27/11/2025

---
+++ UDI_2/Actividades/Boletin_Actvidades/Actividad_3/ej3.js
@@ -0,0 +1,56 @@
+"use strict"
+{
+   let btnMarcar = document.createElement("button");
+    btnMarcar.textContent = "Marcar todos";
+    document.body.appendChild(btnMarcar);
+
+    let btnDesmarcar = document.createElement("button");
+    btnDesmarcar.textContent = "Desmarcar todos";
+    document.body.appendChild(btnDesmarcar);
+
+   let btnAmpliar= document.createElement("button");
+    btnAmpliar.textContent = "Ampliar Tamaño";
+    document.body.appendChild(btnAmpliar);
+
+   let btnRestaurar = document.createElement("button");
+    btnRestaurar.textContent = "Restaurar Tamaño"
+    document.body.appendChild(btnRestaurar);
+    
+    for(let i=0;i<100;i++){
+        let div = document.createElement("div");
+        let input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("value", "N"+Math.floor(Math.random() * 101) + 100);
+        div.appendChild(input);
+        let label = document.createElement("label");
+        label.textContent = i;
+        div.appendChild(label);
+        document.body.appendChild(div);
+    }
+
+
+    btnMarcar.addEventListener("click", function (evento){
+        let miElemento = document.getElementsByTagName("input");
+        for(let elem of miElemento){
+            elem.checked = true;
+            console.log("Elemento marcado" + elem.value);
+        }
+        
+    });
+
+    btnDesmarcar.addEventListener("click", function (evento){
+        let miElemento = document.getElementsByTagName("input");
+        for(let elem of miElemento){
+            elem.checked = false;
+            console.log("Elemento desmarcado" + elem.value);
+        }
+    });
+
+    btnAmpliar.addEventListener("click", function (evento){
+
+    });
+
+    btnRestaurar.addEventListener("click", function (evento){
+
+    });
+}
